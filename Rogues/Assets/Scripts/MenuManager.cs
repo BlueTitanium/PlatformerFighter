d67c151a0b02e86d5e1ecb6cf8ab320563536de9
BlueTitanium <xyzftw@gmail.com>
2020-04-25 18:24:22 -0400

    first edition mainly complete
    todo: music

--- Rogues/Assets/Scripts/MenuManager.cs
+++ Rogues/Assets/Scripts/MenuManager.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MenuManager : MonoBehaviour
 {
+    [SerializeField]
+    public Transform levelSelect = null;
     // Start is called before the first frame update
     void Start()
     {
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
+        levelSelect.localScale = new Vector3(0,0,0);
         Time.timeScale = 1f;
     }
 
@@ -25,9 +28,54 @@ public class MenuManager : MonoBehaviour
         SceneManager.LoadScene("Level0");
         Time.timeScale = 1f;
     }
+    public void LevelSelect(){
+        levelSelect.localScale = new Vector3(1,1,1);
+    }
+    public void ChooseLevel1(){
+        SceneManager.LoadScene("Level1");
+        Time.timeScale = 1f;
+    }
+    public void ChooseLevel2e(){
+        SceneManager.LoadScene("Level2E");
+        Time.timeScale = 1f;
+    }
+    public void ChooseLevel2m(){
+        SceneManager.LoadScene("Level2M");
+        Time.timeScale = 1f;
+    }
+    public void ChooseLevel2h(){
+        SceneManager.LoadScene("Level2H");
+        Time.timeScale = 1f;
+    }
+    public void ChooseLevel3e(){
+        SceneManager.LoadScene("Level3E");
+        Time.timeScale = 1f;
+    }
+    public void ChooseLevel3m(){
+        SceneManager.LoadScene("Level3M");
+        Time.timeScale = 1f;
+    }
+    public void ChooseLevel3h(){
+        SceneManager.LoadScene("Level3H");
+        Time.timeScale = 1f;
+    }
+    public void ChooseLevel4e(){
+        SceneManager.LoadScene("Level4E");
+        Time.timeScale = 1f;
+    }
+    public void ChooseLevel4m(){
+        SceneManager.LoadScene("Level4M");
+        Time.timeScale = 1f;
+    }
+    public void ChooseLevel4h(){
+        SceneManager.LoadScene("Level4H");
+        Time.timeScale = 1f;
+    }
+    public void GoBack(){
+        levelSelect.localScale = new Vector3(0,0,0);
+    }
     public void QuitGame()
     {
         Application.Quit();
     }
-
 }
